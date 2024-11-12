d1c3754ece1281d0432c0cfaf65037be809677c9
Drynia12Kyfeld <121232526+Drynia12Kyfeld@users.noreply.github.com>
2024-11-12 20:52:03 +0300

    Update main.cpp
    Я сделяль

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,29 @@
 #include "lib.hpp"
+#include <thread>
+#include <iostream>
 
 int main() {
-  return 0;
+    int tobaccoSmokedCount = 0;
+    int paperSmokedCount = 0;
+    int matchSmokedCount = 0;
+    int iterationcount = 0;
+    std::cin >> iterationcount;
+
+    std::thread smoker1(smokerWithTobacco, std::ref(tobaccoSmokedCount));
+    std::thread smoker2(smokerWithPaper, std::ref(paperSmokedCount));
+    std::thread smoker3(smokerWithMatch, std::ref(matchSmokedCount));
+    std::thread agentThread(agent, iterationcount);
+
+    smoker1.join();
+    smoker2.join();
+    smoker3.join();
+    agentThread.join();
+
+    std::cout << "result:\n";
+    std::cout << "A smoker with tobacco smoked: " << tobaccoSmokedCount << " cigarettes\n";
+    std::cout << "A smoker with paper smoked: " << paperSmokedCount << " cigarettes\n";
+    std::cout << "A smoker with match smoked: " << matchSmokedCount << " cigarettes\n";
+
+    return 0;
 }
+
