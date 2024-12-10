ddf8f30e525d5ad8a2c19a1d73af30fd93a14243
Drynia12Kyfeld <121232526+Drynia12Kyfeld@users.noreply.github.com>
2024-12-10 18:27:29 +0300

    Update lib.hpp

--- src/lib.hpp
+++ src/lib.hpp
@@ -1,14 +1,17 @@
 
+// lib.hpp
+
 #ifndef LIB_HPP
 #define LIB_HPP
 
 #include <mutex>
 #include <condition_variable>
 
-// Функции для курильщиков
-void smokerWithTobacco(int& tobaccoSmokedCount);
-void smokerWithPaper(int& paperSmokedCount);
-void smokerWithMatch(int& matchSmokedCount);
+// Тип компонента: табак, бумага или спички
+enum Component { TOBACCO, PAPER, MATCH };
+
+// Универсальная функция для курильщика
+void smoker(Component ownedComponent, int& smokedCount);
 
 // Функция для посредника
 void agent(int maxRounds);
